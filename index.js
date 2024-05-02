48e50749498925b576be82f6190f69c738ec69b0
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-05-02 12:09:47 +0200

    bugfix deploy

--- index.js
+++ index.js
@@ -18,10 +18,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.static(__dirname + '/public'));
+
+// app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  // res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 // Your first API endpoint
